1fec384bd9231ce1b2a6035db968a583a3cfdaf1
Manabu <manabu@Manabus-MacBook-Air.local>
2025-07-02 15:21:05 +0900

    feat: update app branding and window dimensions (title, window size, package metadata)

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 const { app, BrowserWindow } = require('electron');
 
-app.whenReady().then(() => {
-  new BrowserWindow().loadFile('index.html');
-});
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1500,
+    height: 1000,
+    minWidth: 1500,
+    minHeight: 1000,
+    center: true,
+    title: 'Just a Paper',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => app.quit()); 
